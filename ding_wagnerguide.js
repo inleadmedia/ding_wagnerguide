e3aa74fbe502bc05f534967c8554dcb714bfd8d7
Jan Hansen <jh@inlead.dk>
2012-01-26 12:32:11 +0000

    comments and whitespace

--- ding_wagnerguide.js
+++ ding_wagnerguide.js
@@ -22,12 +22,14 @@ $(document).ready(function() {
       } else {
         var ajax_path = Drupal.settings.basePath;
       }
-    
+
       // until I can get trampoline to work...
       ajax_path = Drupal.settings.basePath;
-    
+
+      // get the links, and then...
       $.getJSON(ajax_path + 'ding/wagnerguide/item/' + Drupal.dingWagnerguide.itemId, {}, function(data) {
         Drupal.dingWagnerguide.data = data;
+        // ...wait for the ting-availability module to write the holdings in the container
         var waitForContentTimer = window.setInterval(function() {
           if ($('.ting-availability p').length) {
             clearInterval(waitForContentTimer);
@@ -42,8 +44,10 @@ $(document).ready(function() {
 
 Drupal.dingWagnerguide.populate = function() {
   var wagnerLinks = Drupal.dingWagnerguide.data[Drupal.dingWagnerguide.itemId];
+  // Run through the printed holdings and insert links/popups. Hope that the lines are in the same order as our links as we have no id on the individual lines
   $('.ting-availability ul.library-list li').each(function(index){
     if (wagnerLinks[index]) {
+
       if (wagnerLinks[index].popup) {
         var link = $(this).prepend(' <a style="display:inline">[kort]</a> ');
         var $dialog = $('<div></div>').html(wagnerLinks[index].popup).dialog({
@@ -54,13 +58,14 @@ Drupal.dingWagnerguide.populate = function() {
           $dialog.dialog('open');
           return false;
         });
+
       } else if (wagnerLinks[index].href) {
         $(this).prepend('<a style="display:inline" href="' + wagnerLinks[index].href + '" target="_blank">[kort]</a> ');
       }
+
       if (wagnerLinks[index].debug) {
         $(this).append('<!-- ' + wagnerLinks[index].debug + ' -->');
       }
     }
   });
-
 }
